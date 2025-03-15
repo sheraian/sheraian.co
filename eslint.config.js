2e43f69518ea3f016246367dfdc786e74540135e
Muhammad Sheraz <muhammadshiraz492@gmail.com>
2025-03-15 19:52:44 +0500

    starting project

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import unusedImports from "eslint-plugin-unused-imports"; // Add this
+
+export default [
+  { ignores: ["dist"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "unused-imports": unusedImports, // Add this plugin
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      camelcase: ["error", { properties: "always" }],
+      "max-len": ["error", { code: 80, ignoreUrls: true }],
+      "no-unused-vars": ["off"],
+      "unused-imports/no-unused-vars": ["error"],
+      "unused-imports/no-unused-imports": "error",
+    },
+  },
+];
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <img src="/logo.svg" alt="" />
+      </header>
+    </>
+  );
+}
+
+export default App;
