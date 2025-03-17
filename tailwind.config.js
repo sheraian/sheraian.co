86f6d5e0c3b4ce03b15982d612610aaac749f80a
Muhammad Sheraz <muhammadshiraz492@gmail.com>
2025-03-17 07:47:42 +0500

    Blog screen are completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-out forwards",
+      },
+
       fontFamily: {
         poppins: ["Inter", "Poppins", "sans-serif"],
       },
