5581b7b40ea2951eada43b19e061f25b65f00aea
Muhammad Sheraz <muhammadshiraz492@gmail.com>
2025-03-24 06:17:02 +0500

    Website are completly ready

--- src/components/blog/PortfolioHero.jsx
+++ src/components/blog/PortfolioHero.jsx
@@ -1,43 +1,74 @@
+import { motion } from "framer-motion";
 import Btn from "../Btn";
 
-function PortfolioHero({
-  P_BtnText,
-  H_Text,
-  M_Text,
-  isSBtnShow = false,
-  S_BtnText = "",
-}) {
+function PortfolioHero({ P_BtnText, H_Text, M_Text }) {
   const formattedText =
     M_Text.charAt(0).toUpperCase() + M_Text.slice(1).toLowerCase();
 
   return (
-    <div className="w-full h-auto md:h-[550px] bg-[url(/herobg.svg)] bg-cover bg-center flex items-center justify-center py-6 md:py-8 px-4">
-    <div className="w-[90%] mx-auto flex flex-col md:flex-row items-center gap-5 lg:gap-6">
-      
-      <div className="w-full md:w-[60%] flex flex-col items-start gap-4 text-left">
-        
-        <div className="py-2.5 px-8 bg-[#D7D5DF] rounded-full shadow-md hover:bg-[#C5C3CF] transition duration-300 cursor-pointer">
-          <h1 className="text-[#4A2848] font-bold text-lg text-center capitalize">
-            {P_BtnText}
-          </h1>
-        </div>
-  
-        <span className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] font-semibold leading-[1.2] break-words capitalize">
-          {H_Text}
-        </span>
-  
-        <p className="w-full md:w-[80%] text-base sm:text-lg md:text-xl leading-relaxed text-gray-700 capitalize">
-          {formattedText}
-        </p>
+    <motion.div
+      className="w-full h-auto md:h-[550px] bg-[url(/herobg.svg)] bg-cover bg-center flex items-center justify-center py-6 md:py-8 px-4"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
+      <div className="w-[90%] mx-auto flex flex-col md:flex-row items-center gap-5 lg:gap-6">
+      <motion.div
+          className="w-full md:w-[40%] flex md:hidden items-start justify-start"
+          initial={{ x: 50, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          viewport={{ once: true }}
+        >
+          <img
+            src="/ptf_logo.png"
+            className="max-w-full lg:h-[520px] object-contain"
+            alt=""
+          />
+        </motion.div>
+        <motion.div
+          className="w-full md:w-[60%] flex flex-col items-start gap-4 text-left"
+          initial={{ x: -50, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.3 }}
+          viewport={{ once: true }}
+        >
+          <div className="py-2.5 px-8 bg-[#D7D5DF] rounded-full shadow-md hover:bg-[#C5C3CF] transition duration-300 cursor-pointer">
+            <h1 className="text-[#4A2848] font-bold text-lg text-center capitalize">
+              {P_BtnText}
+            </h1>
+          </div>
+
+          <span className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] font-semibold leading-[1.2] break-words capitalize">
+            {H_Text}
+          </span>
+
+          <p className="w-full md:w-[80%] text-base sm:text-lg md:text-xl leading-relaxed text-gray-700 capitalize">
+            {formattedText}
+          </p>
+          <div className="flex gap-2 md:gap-5 flex-wrap">
+            <img src="/playstore.svg" alt="" />
+            <img src="/playstore.svg" alt="" />
+          </div>
+        </motion.div>
+
+        {/* Right Image */}
+        <motion.div
+          className="w-full hidden md:w-[40%] md:flex items-start justify-start"
+          initial={{ x: 50, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          viewport={{ once: true }}
+        >
+          <img
+            src="/ptf_logo.png"
+            className="max-w-full lg:h-[520px] object-contain"
+            alt=""
+          />
+        </motion.div>
       </div>
-  
-      <div className="w-full md:w-[40%] flex items-start justify-start ">
-        <img src="/ptf_logo.png" className="max-w-full lg:h-[520px] object-contain" alt="" />
-      </div>
-  
-    </div>
-  </div>
-  
+    </motion.div>
   );
 }
 
--- src/pages/Portfolio/PortfolioDetailPage.jsx
+++ src/pages/Portfolio/PortfolioDetailPage.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import HeroBlog from "../../components/blog/HeroBlog";
 import { useParams } from "react-router-dom";
-import FaqComponent from "../../components/FaqComponent";
 import DOMPurify from "dompurify";
 import { content } from "../../../data";
-import ReactQuill from "react-quill-new";
 import "react-quill/dist/quill.snow.css";
-import BenefitsCard from "../../components/BenefitsCard";
-import Btn from "../../components/Btn";
-import PortfolioHero from "../../components/blog/PortfolioHero";
 
+import PortfolioHero from "../../components/blog/PortfolioHero";
+import { motion } from "framer-motion";
 function PortfolioDetailPage() {
   const { slug, id } = useParams();
   return (
@@ -23,27 +19,44 @@ function PortfolioDetailPage() {
         isSBtnShow={false}
         S_BtnText={"Explore More"}
       />
-      <div className="w-[90%] mx-auto flex flex-col items-start justify-center text-start gap-5">
-        <img
+      <motion.div
+        className="w-[90%] mx-auto flex flex-col items-start justify-center text-start gap-5"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <motion.img
           src="/frontend.png"
           alt="Blog Image"
           className="w-full h-auto max-h-[500px] object-cover rounded-xl"
+          initial={{ scale: 0.9, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          viewport={{ once: true }}
         />
-        <span
+
+        <motion.span
           className="w-full text-[1.5rem] sm:text-[2.0rem] md:text-[2.5rem] lg:text-[3.0rem] text-start font-semibold 
-  leading-[2.5rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem] break-words capitalize"
+        leading-[2.5rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.0rem] break-words capitalize"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.4 }}
+          viewport={{ once: true }}
         >
           {slug}
-        </span>
-        <div
-          className="ql-editor w-full v text-base md:text-lg leading-relaxed"
-          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
-        />
-      </div>
-    
+        </motion.span>
 
- 
-      
+        {/* Blog Content */}
+        <motion.div
+          className="ql-editor w-full text-base md:text-lg leading-relaxed"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.6 }}
+          viewport={{ once: true }}
+        />
+      </motion.div>
     </div>
   );
 }
